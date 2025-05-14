e2a7fb052bc6d8df1ae1046bc9e5d74016dcb432
Aruna Devi <arunaravimurugan@gmail.com>
2025-05-14 18:42:11 +0530

    Gaming-site

---
+++ award/tailwind.config.js
@@ -0,0 +1,32 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         zentry: ["zentry", "sans-serif"],
+//         general: ["general", "sans-serif"],
+//         circular: ["circular-web", "sans-serif"],
+//         medium: ["robert-medium", "sans-serif"],
+//         regular: ["robert-regular", "sans-serif"],
+//       },
+//       colors: {
+//         blue: {
+//           50: "#DFDFF0",
+//           75: "#dfdff2",
+//           100: "#F0F2FA",
+//           200: "#010101",
+//           300: "#4FB7DD",
+//         },
+//         violett: {
+//           300: "#5724ff",
+//         },
+//         yellow: {
+//           100: "#8e983f",
+//           300: "#edff66",
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
